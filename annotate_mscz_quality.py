0a3bd02d868c547804f8932170193ba0431765d0
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2023-03-24 17:31:06 +0100

    Extract Mozart and Beethoven scores

--- annotate_mscz_quality.py
+++ annotate_mscz_quality.py
@@ -48,8 +48,8 @@ def filter_piano(filenames, metadata):
             except KeyError:
                 pass
 
-    piano_only = []
-    with_piano = []
+    beethoven = []
+    mozart = []
     # Iterate over the filenames
     for filename in tqdm(filenames):
         # Extract the ID from the filename
@@ -58,12 +58,13 @@ def filter_piano(filenames, metadata):
         if file_id in lookup:
             # Check if the instrumentsNames field contains "Piano"
             if ["Piano"] == lookup[file_id]["instrumentsNames"]:
-                piano_only.append(filename)
-            elif "Piano" in lookup[file_id]["instrumentsNames"]:
-                with_piano.append(filename)
+                if 'beethoven' in lookup[file_id]["description"].lower():
+                    beethoven.append(filename)
+                elif 'mozart' in lookup[file_id]["description"].lower():
+                    mozart.append(filename)
     print(
-        f'Got {len(piano_only)} piano-only scores and {len(with_piano)} scores that contain piano')
-    return piano_only, with_piano
+        f'Got {len(mozart)} Mozart scores and {len(beethoven)} Beethoven scores')
+    return mozart, beethoven
 
 
 def get_mscz_paths(dir_path):
@@ -79,36 +80,36 @@ def get_mscz_paths(dir_path):
     return file_list
 
 
-def annotate_scores(file_list):
-    # Create empty pandas DataFrame to store annotations
-    annotations_df = pd.DataFrame(columns=['filename', 'quality'])
-    annotations_df = annotations_df.set_index('filename')
-    # Iterate through files and annotate
-    for filename in file_list:
-        # Open file with Musescore
+def mscz2musicxml(scores, json_name):
+    to_discard = []
+    pattern = r"\.\./MuseScore/\d+/\d+\.mscz"
+    json_batch = create_convert_batch(scores, to_discard)
+    with open(json_name, 'w') as f:
+        json.dump(json_batch, f)
+    while True:
         mscore_process = subprocess.Popen(
-            ['musescore.mscore', filename],
-            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            ['musescore.mscore', '-j', json_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print('Waiting to finish')
+        mscore_process.wait()
+        print('Done')
 
-        print(f"Enter annotation for {filename} (g = good, b = bad, n = idk):")
-
-        # Prompt user to input annotation
-        annotation = None
-        while annotation not in ['g', 'b', 'n', 'q']:
-            annotation = getch.getch()
-        print(f"Annotation for {filename}: {annotation}")
-
-        # Add annotation to DataFrame
-        if annotation == 'g':
-            annotations_df.loc[filename] = {'quality': True}
-        elif annotation == 'b':
-            annotations_df.loc[filename] = {'quality': False}
-        elif annotation == 'n':
-            annotations_df.loc[filename] = {'quality': None}
-        elif annotation == 'q':
-            print("Stopping program early and saving csv")
+        # Get last error line
+        err_out = mscore_process.stderr.read().decode()
+        matches = re.findall(pattern, err_out)
+        if len(matches) != 0:
+            problematic_file = matches[-1]
+            print(problematic_file)
+            if problematic_file not in to_discard:
+                print(f"Discarding {problematic_file}")
+                to_discard.append(problematic_file)
+        # Add file that's problematic to the discard list
+        print(to_discard)
+        json_batch = create_convert_batch(scores, to_discard)
+        if len(json_batch) == 0:
             break
-    return annotations_df
+        with open(json_name, 'w') as f:
+            json.dump(json_batch, f)
+        print(f"Files to process: {len(json_batch)}")
 
 
 def create_convert_batch(score_list, to_discard):
@@ -131,50 +132,6 @@ def create_convert_batch(score_list, to_discard):
         json_out.append(output)
     print(f"Job will process {len(json_out)} files")
     return json_out
-
-
-def count_musicxml(file_list):
-    count = 0
-
-    for file in file_list:
-        musicxml_file = os.path.splitext(file)[0] + '.musicxml'
-        if os.path.exists(musicxml_file):
-            count += 1
-
-    print(f'Total number of files with corresponding musicxml: {count}')
-
-
-def mscz2musicxml(piano_only_scores):
-    to_discard = []
-    pattern = r"\.\./MuseScore/\d+/\d+\.mscz"
-    json_batch = create_convert_batch(piano_only_scores, to_discard)
-    with open('data.json', 'w') as f:
-        json.dump(json_batch, f)
-    while True:
-        mscore_process = subprocess.Popen(
-            ['musescore.mscore', '-j', 'data.json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print('Waiting to finish')
-        mscore_process.wait()
-        print('Done')
-
-        # Get last error line
-        err_out = mscore_process.stderr.read().decode()
-        matches = re.findall(pattern, err_out)
-        if len(matches) != 0:
-            problematic_file = matches[-1]
-            print(problematic_file)
-            if problematic_file not in to_discard:
-                print(f"Discarding {problematic_file}")
-                to_discard.append(problematic_file)
-        # Add file that's problematic to the discard list
-        print(to_discard)
-        json_batch = create_convert_batch(piano_only_scores, to_discard)
-        if len(json_batch) == 0:
-            break
-        with open('data.json', 'w') as f:
-            json.dump(json_batch, f)
-        count_musicxml(piano_only_scores)
-        print(f"Files to process: {len(json_batch)}")
 
 
 def get_musicxml_paths(file_list):
@@ -213,44 +170,48 @@ def filter_empty(scores):
     return filtered_paths
 
 
+def create_filtered_pickle(filename, obj):
+    filtered_musicxml = None
+    if not os.path.exists(filename):
+        filtered_musicxml = filter_empty(obj)
+        with open(filename, 'wb') as f:
+            pickle.dump(filtered_musicxml, f)
+    else:
+        with open(filename, 'rb') as f:
+            filtered_musicxml = pickle.load(f)
+    return filtered_musicxml
+
+
 def main():
     args = parse_args()
     if args.process:
         file_list = get_mscz_paths(args.dir_path)
-        piano_only_scores, with_piano_scores = filter_piano(
+        mozart, beethoven = filter_piano(
             file_list, args.metadata)
 
         # Store piano-only and with piano files in pickle files
-        with open('piano_only.pkl', 'wb') as f:
-            pickle.dump(piano_only_scores, f)
+        with open('mozart.pkl', 'wb') as f:
+            pickle.dump(mozart, f)
 
-        with open('with_piano.pkl', 'wb') as f:
-            pickle.dump(with_piano_scores, f)
+        with open('beethoven.pkl', 'wb') as f:
+            pickle.dump(beethoven, f)
 
-    # Load the list from the file
-    with open('piano_only.pkl', 'rb') as f:
-        piano_only_scores = pickle.load(f)
+    with open('mozart.pkl', 'rb') as f:
+        mozart = pickle.load(f)
+    with open('beethoven.pkl', 'rb') as f:
+        beethoven = pickle.load(f)
 
     if args.convert:
-        mscz2musicxml(piano_only_scores)
+        #mscz2musicxml(mozart, 'mozart.json')
+        mscz2musicxml(beethoven, 'beethoven.json')
 
-    filtered_musicxml_paths = None
-    if not os.path.exists('filtered.pkl'):
-        filtered_musicxml_paths = filter_empty(piano_only_scores)
-        count_musicxml(filtered_musicxml_paths)
-        with open('filtered.pkl', 'wb') as f:
-            pickle.dump(filtered_musicxml_paths, f)
-    else:
-        with open('filtered.pkl', 'rb') as f:
-            filtered_musicxml_paths = pickle.load(f)
+    filtered_musicxml_mozart = create_filtered_pickle(
+        'filtered_mozart', mozart)
+    filtered_musicxml_beethoven = create_filtered_pickle(
+        'filtered_beethoven', beethoven)
 
-    annotations_df = annotate_scores(filtered_musicxml_paths)
-
-    # Save annotations to CSV file
-    annotations_df.to_csv(args.csv_path, index=True)
-
-    print("Annotations:")
-    print(annotations_df)
+    print(f"There is {filtered_musicxml_mozart} Mozart files")
+    print(f"There is {filtered_musicxml_beethoven} Beethoven files")
 
 
 if __name__ == "__main__":
